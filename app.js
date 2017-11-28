24ddaa3b0fcca8168a67272745b12df4010ea214
Manjunath-Garag <32258833+Manjunath-Garag@users.noreply.github.com>
2017-11-28 10:12:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+var index = require('./routes/restaurants');
+var users = require('./routes/book_del');
 
 var dbConn=require('./Config/mysqlConnection');
 
@@ -24,8 +24,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.use('/', restaurants);
+app.use('/users', book_del);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
