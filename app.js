92737e6c18514e3686001957e614acc1f8184ef3
Manjunath-Garag <32258833+Manjunath-Garag@users.noreply.github.com>
2017-11-28 10:17:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', restaurants);
-app.use('/users', book_del);
+app.use('/book_del', book_del);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
